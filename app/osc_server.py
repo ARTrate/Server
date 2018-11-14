fcb91cbba2161ac8137d22795d061209572eb206
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2018-11-14 23:02:03 +0100

    Formatting

--- app/osc_server.py
+++ app/osc_server.py
@@ -6,17 +6,15 @@ def dispatchEffectEngines(addr, args):
   print("+++++++ RECEIVED OSC DATA +++++++ ")
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--ip",
-      default="app", help="The ip to listen on")
-  parser.add_argument("--port",
-      type=int, default=5005, help="The port to listen on")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="app", help="The ip to listen on")
+    parser.add_argument("--port", type=int, default=5005, help="The port to listen on")
+    args = parser.parse_args()
   
-dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/bpm", dispatchEffectEngines)
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map("/bpm", dispatchEffectEngines)
 
-server = osc_server.ThreadingOSCUDPServer(
-      (args.ip, args.port), dispatcher)
-print("Serving on {}".format(server.server_address))
-server.serve_forever()
+    server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
+
