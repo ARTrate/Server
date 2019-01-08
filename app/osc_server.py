1c743e5166a3268cfe4b72b6cfb19643b50ad9ac
Tamara Bernhardt <bernhardt.tamara@gmx.de>
2019-01-08 17:34:13 +0100

    changed def ip to localhost since docker is not used anymore

--- app/osc_server.py
+++ app/osc_server.py
@@ -83,7 +83,7 @@ def postProcessRR(addr, args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="app", help="The ip to listen on")
+    parser.add_argument("--ip", default="localhost", help="The ip to listen on")
     parser.add_argument("--port", type=int, default=5005,
                         help="The port to listen on")
     args = parser.parse_args()
