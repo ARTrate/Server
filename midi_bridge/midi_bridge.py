3aed16d19fa94bf39937f72c85a93099011806a7
Maximilian Rohland <max@rohland.net>
2019-02-06 16:40:57 +0100

    started work on midi bridge

---
+++ midi_bridge/midi_bridge.py
@@ -0,0 +1,26 @@
+import sys
+from pythonosc import dispatcher, osc_server
+import argparse
+
+
+def bridge_to_midi(addr, value):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="localhost",
+                        help="The ip to listen on")
+    parser.add_argument("--port", type=int, default=5005,
+                        help="The port to listen on")
+    args = parser.parse_args()
+
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map("/midi", bridge_to_midi)
+
+    server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    try:
+        server.serve_forever()
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit()
